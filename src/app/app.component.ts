06813f1187594de0740d058b540d83da040a1921
GregVaz <grvazquezya@ittepic.edu.mx>
2020-06-05 15:18:01 -0600

    Pipe personalizado: capitalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Gregorio';
+  nombre2: string = 'StrNGJ MejoWDs HiMEnez';
 
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
